f7a6bd057d0cfd32f14aa4a0f62c6b9d88fcd99d
WoutNerd <96572746+WoutNerd@users.noreply.github.com>
2022-10-17 21:15:34 +0200

    gitignore + navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <p className="leerkracht">Leerkracht</p>
       <p className="leerling">Leerling</p>
     </div>
