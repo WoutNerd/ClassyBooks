db95d2e714323747ece436f2b9eec2efbcc91b07
WoutNerd <96572746+WoutNerd@users.noreply.github.com>
2022-11-29 10:40:16 +0100

    teacher login button translated class choise in array

--- client/src/pages/class.jsx
+++ client/src/pages/class.jsx
@@ -1,17 +1,23 @@
 import "../App.css"
 
+// class names
+const classNames = [
+{name :"wolfklas",imgUrl: "https://img.static-rmg.be/a/view/q75/w1200/h800/3438717/88d668dab0dc1a68da347f15bf0b85fe-jpg.jpg",},
+{name :"girafklas", imgUrl: "https://d33b12c77av9bg.cloudfront.net/originals/veulen-giraffen-max-jacques-beeksebergen-resort-safari-giraffe.jpg"},
+{name :"girafklas", imgUrl: "https://d33b12c77av9bg.cloudfront.net/originals/veulen-giraffen-max-jacques-beeksebergen-resort-safari-giraffe.jpg"},
+];
 
 const Class = () => {
     return ( 
-        <div className="klas">
+         <div className="klas">
             <ul className="klas">
-                <li className="klas">
-                    <p>Wolfklas</p>
-                    <img src="https://img.static-rmg.be/a/view/q75/w1200/h800/3438717/88d668dab0dc1a68da347f15bf0b85fe-jpg.jpg" alt="" />
-                </li>
-                <li>
-                    <p></p>
-                </li>
+            {classNames.map((item)=>{
+                 return (
+                  <li className="klas">{item.name} <br />
+                  <img src={item.imgUrl} alt="" />
+                  </li>
+                 )
+               })}
             </ul>
         </div>
      );
