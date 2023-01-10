15c7fbf0cad7543c631bc5dc10ce7f4f4b3fe531
Jasper De Gussemé <jasperdegus@gmail.com>
2023-01-10 11:10:26 +0100

    Added SQL Requests

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 //-------------------------------------------------------------------------------------IMPORTS-------//
 const express = require("express");
+const fs = require("fs");
 const PORT = process.env.PORT || 3001;
 const app = express();
+var crypto = require("crypto");
+const { Sequelize } = require("sequelize-cockroachdb");
+
 //-------------------------------------------------------------------------------------REQUESTS-----//
 app.use(express.static(__dirname + "/../client/build/"));
 app.listen(PORT, () => {
@@ -10,98 +14,56 @@ app.listen(PORT, () => {
 app.get("*", (req, res) => {
   res.sendFile(__dirname + "/../client/build/index.html");
 });
+//------------------------------------------------------------------------------------SQL SERVER----//
+settings = JSON.parse(fs.readFileSync("./server/settings.json"));
+console.log(settings["url"]);
+
+const sequelize = new Sequelize(settings["url"]);
+
+async function request(request) {
+  try {
+    const [results, metadata] = await sequelize.query(request);
+    return [results, metadata];
+  } catch (err) {
+    return err;
+  } finally {
+    await sequelize.close();
+  }
+}
+
 //-------------------------------------------------------------------------------------FUNCTIONS----//
-function importData(json) {
-  let d = JSON.parse(json);
-  let dtemp = { books: [], accounts: [] };
-  for (let i = 0; i < d["books"].length; i++) {
-    dtemp["books"].append(
-      new Material(
-        d["books"][i]["id"],
-        d["books"][i]["title"],
-        d["books"][i]["place"],
-        d["books"][i]["description"],
-        d["books"][i]["available"]
-      )
-    );
-  }
-  for (let i = 0; i < d["accounts"].length; i++) {
-    dtemp["accounts"].append(
-      new Account(
-        d["accounts"][i]["id"],
-        d["accounts"][i]["name"],
-        d["accounts"][i]["surname"],
-        d["accounts"][i]["privileged"],
-        d["accounts"][i]["sha256"],
-        d["accounts"][i]["md5"],
-        d["accounts"][i]["materials"]
-      )
-    );
-  }
-  return dtemp;
+function generateHashes(name, surname, password) {
+  var sha256 = crypto
+    .createHash("sha256")
+    .update(name + surname + password)
+    .digest("hex");
+  var md5 = crypto
+    .createHash("md5")
+    .update(name + surname + password + sha256.toString())
+    .digest("hex");
+  return [sha256.toString(), md5.toString()];
 }
 
-function exportData(d) {
-  return JSON.stringify(d);
+function addUserWithPass(name, surname, password, privileged, materials) {
+  console.log("a");
+  let privilegedBit = 0;
+  if (privileged) {
+    privilegedBit = 1;
+  }
+  let hashes = generateHashes(name, surname, password);
+  req = new Request(
+    `INSERT INTO USERS VALUES(${name}, ${surname}, ${privilegedBit}, ${hashes[0]}, ${hashes[1]}, '')`,
+    (err) => {
+      console.log(err);
+    }
+  );
+  connection.execSql(req);
+  req.on("requestCompeleted", () => {
+    console.log("Request completed");
+  });
 }
-//-------------------------------------------------------------------------------------CLASSES-----//
-class Material {
-  constructor(id, title, place, description, available) {
-    this.id = id;
-    this.title = title;
-    this.place = place;
-    this.description = description;
-    this.available = available;
-  }
-  getJson() {
-    return {
-      id: this.id,
-      title: this.title,
-      place: this.place,
-      description: this.description,
-      available: this.available,
-    };
-  }
-}
-class Account {
-  constructor(id, name, surname, privileged, sha256, md5, materials) {
-    this.id = id;
-    this.name = name;
-    this.surname = surname;
-    this.privileged = privileged;
-    this.sha256 = sha256;
-    this.md5 = md5;
-    this.materials = materials;
-  }
-  getJson() {
-    return {
-      id: this.id,
-      name: this.name,
-      surname: this.surname,
-      privileged: this.privileged,
-      sha256: this.sha256,
-      md5: this.md5,
-      materials: this.materials,
-    };
-  }
-}
-
 //--------------------------------------------------------------------------------------------------//
-d = {
-  accounts: [
-    new Account(
-      "ID",
-      "Name",
-      "Surname",
-      false,
-      "d10207ff4e3546fcae95062ba7382ae1a55cdea3d758a98c8cc8b179e170f74e",
-      "5b3270619a04fff5ac5790680ac65956",
-      []
-    ),
-  ],
-  books: [
-    new Material("0", "Arabische sprookjes", "Prentenboeken & Sprookjes", { "isbn": "9789025771614", "author": "Rodhan Al Galidi", "pages": 136 })
-  ]
-};
-
-console.log(exportData(d))
+(async () => {
+  const a = await request("SELECT * FROM USERS");
+  console.log(a[0]);
+})();
