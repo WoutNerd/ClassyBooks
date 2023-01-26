c94693121f17b8099ff597b6eebc578e5e4f308b
WoutNerd <96572746+WoutNerd@users.noreply.github.com>
2023-01-26 10:08:00 +0100

    teacher navbar redirects

--- client/src/pages/teacherNavbar.jsx
+++ client/src/pages/teacherNavbar.jsx
@@ -5,15 +5,15 @@ const TeacherNavbar = () => {
         <div className="lkNavbarDiv">
             <ul className="lkNavbar">
                 <li className="lkNavbar">
-                    <img src={require('../images/Dashboard.png')} alt="Dashboard" className="lkNavbar"/>
+                    <a href="./overzicht"><img src={require('../images/Dashboard.png')} alt="Dashboard" className="lkNavbar" /></a>
                 </li>
                 <br />
                 <li className="lkNavbar">
-                    <img src={require('../images/Graduates.png')} alt="Leerlingen" className="lkNavbar"/>
+                    <a href="/leerlingen"><img src={require('../images/Graduates.png')} alt="Leerlingen" className="lkNavbar"/></a>
                 </li>
                 <br />
                 <li className="lkNavbar">
-                    <img src={require('../images/book.png')} alt="Bibliotheek" className="lkNavbar"/>
+                    <a href="./bibliotheek"><img src={require('../images/book.png')} alt="Bibliotheek" className="lkNavbar"/></a>
                 </li>
             </ul>
        </div>
