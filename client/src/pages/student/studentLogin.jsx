f160b8761ffb547d7431d01b840421eb46ebd424
WoutNerd <96572746+WoutNerd@users.noreply.github.com>
2023-05-04 14:13:03 +0200

    student login fixed

--- client/src/pages/student/studentLogin.jsx
+++ client/src/pages/student/studentLogin.jsx
@@ -2,26 +2,32 @@ import "../../App.css"
 import { post, Title } from "../../functions"
 import crypto from "crypto-js"
 
+async function handleSubmit(event) {
+    event.preventDefault();
+    var { clss, number, pass } = document.forms[0];
+    clss = clss.value
+    number = number.value
+    pass = pass.value
+
+    var sha256 = crypto.SHA256(clss+number+pass).toString();
+    var md5 = crypto.MD5(clss+number+pass+sha256).toString();
+    const body = {clss, number, sha256, md5};
+    const response = await post('/loginPupil', body)
+    console.log(response.privilege)
+    if (response.privilege != 0) {
+        alert("Leerkrachten en beheerders moeten zich inloggen via de loginpagina voor leerkrachten")
+   // window.location.replace('../leerkracht')
+    }else if (response.privilege === 0) {
+        document.cookie = "sessionId=" + response.sessionid + ";path=../";
+        document.cookie = "userId=" + response.userid + ";path=../"
+        window.location.replace('./boeken')
+    }
+}
+
 const StudentLogin = () => {
     Title("Leerling login")
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        var { clss, number, pass } = document.forms[0];
 
-        var sha256 = crypto.SHA256(clss.value+number.value+pass.value).toString();
-        var md5 = crypto.MD5(clss.value+number.value+pass.value+sha256).toString();
-        const body = {clss, number, sha256, md5};
-        const response = post('/loginPupil', body)
-        if (response.privilege != 0) {
-            alert("Leerkrachten en beheerders moeten zich inloggen via de loginpagina voor leerkrachten")
-            window.location.replace('../leerkracht')
-        }else if (response.privilege === 0) {
-            document.cookie = "sessionId=" + response.sessionid + ";path=../";
-            document.cookie = "userId=" + response.userid + ";path=../"
-        }
-
-    }
 
     //Login form
     const renderForm = (
