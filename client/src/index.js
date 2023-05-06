5df5acd1f6281672903f02dfb690ef1c0862d543
Wout  Van den Bossche <woutvdb@icloud.com>
2023-05-06 22:19:18 +0200

    page that lists all users+option to delete a user

--- client/src/index.js
+++ client/src/index.js
@@ -17,6 +17,7 @@ import UserTyperChoise from './userTypeChoise';
 import AddMaterial from './pages/admin/addMaterial';
 import StudentLib from './pages/student/studentLibrary';
 import Test from "./pages/test";
+import ManageUsers from './pages/admin/manageUsers';
 
 
 
@@ -31,8 +32,9 @@ root.render(
         <Route path='leerkracht' element={<TeacherLogin />} />
         <Route path='leerkracht/overzicht' element={<Dashboard/>}/>
         <Route path='leerkracht/bibliotheek' element={<TeacherLib/>}/>
-        <Route path='leerkracht/gebruiker-toevoegen' element={<AddUser/>}/>
-        <Route path='leerkracht/boek-toevoegen' element={<AddMaterial/>}/>
+        <Route path='beheer/gebruiker-toevoegen' element={<AddUser/>}/>
+        <Route path='beheer/gebruikers=beheren' element={<ManageUsers/>}/>
+        <Route path='beheer/boek-toevoegen' element={<AddMaterial/>}/>
         <Route path='leerling/bibliotheek' element={<StudentLib/>}/>
         <Route path='test' element={<Test/>}/>
       </Route>
---
+++ client/src/pages/admin/manageUsers.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import '../../App.css'
+import { getCookie, Title, post, checkUser } from '../../functions';
+
+async function deleteUser(userId) {
+    const sessionId = getCookie('sessionId')
+    const body = {sessionId, userId}
+    if(window.confirm('Weet u zeker dat u deze gebruiker wilt verwijderen?')){
+        post('/removeUser', body)
+    }else{}
+}
+
+const ManageUsers =  () => {
+    Title('Gebruikers beheren')
+    checkUser(2)
+    
+    const [users, setUsers] = useState(null)
+    const [selectedUser, setSelectedUser] = useState(null)
+    const [showAll, setShowAll] = useState(true)
+    const sessionId = getCookie('sessionId')
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+        const body = {sessionId}
+        const response = await post("/allUsers", body)
+        setUsers(response);
+        } catch (error) {
+          console.error(error)
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
+  
+    if (!users) {
+      return <div>Loading...</div>;
+    }
+  
+    return (<div>
+        <div>
+            {showAll ? 
+        users.map((user) => (
+          <div key={user.userid}>
+            <h3 onClick={() => { setSelectedUser(user); setShowAll(false); }}>{user.firstname +' '+ user.lastname}</h3>
+          </div>
+        ))
+        : <div>
+            <h2>{selectedUser.firstname+' '+selectedUser.lastname}</h2>
+            <button onClick={() => {}}>Verander wachtwoord van deze {selectedUser.firstname+' '+selectedUser.lastname}</button>
+            <button onClick={() => {deleteUser(selectedUser.userid)}}>Verwijder {selectedUser.firstname+' '+selectedUser.lastname}</button>
+            <button onClick={() => setShowAll(true)} className="button">Toon alle gebruikers</button>
+          </div>
+          }
+        </div>
+      </div> 
+    )
+  };
+  
+  export default ManageUsers;
