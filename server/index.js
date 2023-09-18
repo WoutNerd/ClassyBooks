ff1cedb8b21825d8bda5673c9735b4a4497e817c
Jasper De Gussemé <jasperdegus@gmail.com>
2023-09-18 11:55:12 +0200

    Port 3001-->8080

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 //-------------------------------------------------------------------------------------IMPORTS-------//
 const express = require("express");
 const fs = require("fs");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 const app = express();
 var crypto = require("crypto");
 const { Sequelize } = require("sequelize-cockroachdb");
