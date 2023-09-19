1f67ed9e98012963d81a8bfbc93612b461c5c5a1
WoutNerd <96572746+WoutNerd@users.noreply.github.com>
2023-09-19 20:00:33 +0200

    student login fixed

--- client/src/pages/student/studentLogin.jsx
+++ client/src/pages/student/studentLogin.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import "../../App.css"
 import { post, Title } from "../../functions"
 import crypto from "crypto-js"
@@ -13,31 +12,27 @@ const StudentLogin = () => {
     const redirectToPage = (path) => {
       navigate(path); // Use navigate to go to the specified path
     };
-    function HandleSubmit(event) {
-    useEffect( ()=> {
-      async function fetchData() {
-        event.preventDefault();
-        var { clss, number, pass } = document.forms[0];
-        clss = clss.value
-        number = number.value
-        pass = pass.value
+
+    const HandleSubmit = async (event) => {
+      event.preventDefault();
+      var { clss, number, pass } = document.forms[0];
+      clss = clss.value
+      number = number.value
+      pass = pass.value
     
-        var sha256 = crypto.SHA256(clss+number+pass).toString();
-        var md5 = crypto.MD5(clss+number+pass+sha256).toString();
-        const body = {clss, number, sha256, md5};
-        const response = await post('/loginPupil', body)
-        console.log(response.privilege)
-        if (response.privilege !== 0) {
-            alert("Leerkrachten en beheerders moeten zich inloggen via de loginpagina voor leerkrachten")
-        }else if (response.privilege === 0) {
-            document.cookie = "sessionId=" + response.sessionid + ";path=../";
-            document.cookie = "userId=" + response.userid + ";path=../"
-            redirectToPage('./boeken')
-        }} fetchData();
-    })
-  }
-
-
+      var sha256 = crypto.SHA256(clss+number+pass).toString();
+      var md5 = crypto.MD5(clss+number+pass+sha256).toString();
+      const body = {clss, number, sha256, md5};
+      const response = await post('/loginPupil', body)
+      console.log(response.privilege)
+      if (response.privilege !== 0) {
+          alert("Leerkrachten en beheerders moeten zich inloggen via de loginpagina voor leerkrachten")
+      }else if (response.privilege === 0) {
+          document.cookie = "sessionId=" + response.sessionid + ";path=../";
+          document.cookie = "userId=" + response.userid + ";path=../"
+          redirectToPage('./bibliotheek')
+    }
+    }
     //Login form
     const renderForm = (
         <div className="form">
