340ca03170d40a7d3648231fcc36a4ddcf3a80b6
WoutNerd <96572746+WoutNerd@users.noreply.github.com>
2023-10-05 13:40:32 +0200

    go back btn added to nopage

--- client/src/pages/NoPage.jsx
+++ client/src/pages/NoPage.jsx
@@ -2,7 +2,7 @@ import "../App.css";
 
 const NoPage = () => {
     return ( 
-        <h1>Deze pagina is niet gevonden.<br/> Hij is mogelijk verplaatst of nog in aanbouw.</h1>
+        <h1>Deze pagina is niet gevonden.<br/> Hij is mogelijk verplaatst of nog in aanbouw.<br/> <button onClick={()=>{window.history.back()}} className="button">Ga terug</button></h1>
      );
 }
  
