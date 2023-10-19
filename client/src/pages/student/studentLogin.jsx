8fad601c2a046ff8836cb24935b9969c2bf08a16
WoutNerd <96572746+WoutNerd@users.noreply.github.com>
2023-10-19 15:05:45 +0200

    console/log removed

--- client/src/pages/student/studentLogin.jsx
+++ client/src/pages/student/studentLogin.jsx
@@ -15,7 +15,6 @@ const StudentLogin = () => {
 
     const request = async (body) => {
       const response = await post('/loginPupil', body)
-      console.log(response.privilege)
       if (response.privilege !== 0) {
           alert("Leerkrachten en beheerders moeten zich inloggen via de loginpagina voor leerkrachten")
       }else if (response.privilege === 0) {
