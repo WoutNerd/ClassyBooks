3f05e6852d2ae83d98a094522777d1188f033b40
Jasper De Gussemé <jasperdegus@gmail.com>
2023-11-16 13:21:23 +0100

    New SQL-injection detection

--- server/index.js
+++ server/index.js
@@ -281,7 +281,26 @@ function generateHashes(name, surname, password) {
 }
 function checkString(str) {
   //Returns false when string is not good
-  return !(new RegExp(/(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE|UNION( +ALL){0,1})/, "gi").test(str))
+  var sql_meta = new RegExp('(%27)|(\')|(--)|(%23)|(#)', 'i');
+  if (sql_meta.test(value)) {
+    return false;
+  }
+
+  var sql_meta2 = new RegExp('((%3D)|(=))[^\n]*((%27)|(\')|(--)|(%3B)|(;))', 'i');
+  if (sql_meta2.test(value)) {
+    return false;
+  }
+
+  var sql_typical = new RegExp('w*((%27)|(\'))((%6F)|o|(%4F))((%72)|r|(%52))', 'i');
+  if (sql_typical.test(value)) {
+    return false;
+  }
+
+  var sql_union = new RegExp('((%27)|(\'))union', 'i');
+  if (sql_union.test(value)) {
+    return false;
+  }
+  return true
 }
 function checkRequest(req) {
   let stringGood = true
