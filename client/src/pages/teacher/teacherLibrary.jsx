835b9fef8d1a98a18d178649010c16b46a81d4a6
WoutNerd <96572746+WoutNerd@users.noreply.github.com>
2023-11-24 11:59:03 +0100

    all libraries added

--- client/src/pages/teacher/teacherLibrary.jsx
+++ client/src/pages/teacher/teacherLibrary.jsx
@@ -7,12 +7,15 @@ import {checkUser, post, Title} from '../../functions'
 const TeacherLib =  () => {
   Title('Bibliotheek')
   checkUser(1)
-  
   const [books, setBooks] = useState(null);
+  const [filterdBooks, setFilterdBooks] = useState(null)
   const [showAll, setShowAll] = useState(true);
   const [selectedBook, setSelectedBook] = useState(null);
   const [sort, setSort] = useState('title')
   const [sortDirection, setSortDirection] = useState('ascending')
+  const [filter, setFilter] = useState('none')
+  const [locations, setLocations] = useState([])
+  const [readinglevels, setReadinglevels] = useState([])
 
 
   useEffect(() => {
@@ -20,6 +23,8 @@ const TeacherLib =  () => {
       try {
         const response = await post("/allMaterials")
         setBooks(response);
+        setFilterdBooks(response)
+        
       } catch (error) {
         console.error(error)
       }
@@ -29,8 +34,51 @@ const TeacherLib =  () => {
   }, []);
 
 
+  books?.map(book => {
+    if(!locations.includes(book.place)){
+      setLocations([...locations, book.place])
+    }
+  })
+
+  books?.map(book => {
+    if(!readinglevels.includes(book.descr.readinglevel)){
+      setReadinglevels([...readinglevels, book.descr.readinglevel])
+    }
+  })
+
+  console.log(readinglevels)
+  console.log(locations)
+
   if (!books) {
     return <div>Loading...</div>;
+  }
+
+  const handleChangeFilter = (event) => {
+    const {
+      selectedIndex,
+      options
+    } = event.currentTarget;
+    const selectedOption = options[selectedIndex];
+    const selectedFilter = selectedOption.value;
+    const selectedFilterGroup = selectedOption.closest('optgroup')?.id;
+
+    setFilter(selectedFilter)
+    if(selectedFilterGroup === 'place'){
+      const selectedFilterBooks = books.filter(book => book.place.includes(selectedFilter))
+      setFilterdBooks(selectedFilterBooks)
+    }
+    if(selectedFilterGroup === 'readinglevel'){
+      const selectedFilterBooks =  books.filter(book => book.descr.readinglevel.includes(selectedFilter))
+      setFilterdBooks(selectedFilterBooks)
+      
+    }
+    if(selectedFilterGroup === 'available'){
+      const selectedFilterBooks = books.filter(book => book.available.includes(selectedFilter))
+      setFilterdBooks(selectedFilterBooks)
+    } 
+
+    if (selectedFilter === 'none') setFilterdBooks(books)
+    
   }
 
   const handleChangeSort = (event) => {
@@ -39,7 +87,7 @@ const TeacherLib =  () => {
   
     setSort(selectedSort)
   
-    const sortedMaterials = [...books].sort((a, b) => {
+    const sortedMaterials = [...filterdBooks].sort((a, b) => {
       if (selectedDirection === 'ascending') {
         if (a[selectedSort] < b[selectedSort]) return -1;
         if (a[selectedSort] > b[selectedSort]) return 1;
@@ -48,10 +96,10 @@ const TeacherLib =  () => {
         if (a[selectedSort] > b[selectedSort]) return -1;
         if (a[selectedSort] < b[selectedSort]) return 1;
         return 0;
-      }
+      } return null
     });
   
-    setBooks(sortedMaterials); // Update the sorted data
+    setFilterdBooks(sortedMaterials); // Update the sorted data
   };
   
   
@@ -62,7 +110,7 @@ const TeacherLib =  () => {
   
     setSortDirection(selectedDirection); // Update the sort direction
   
-    const sortedMaterials = [...books].sort((a, b) => {
+    const sortedMaterials = [...filterdBooks].sort((a, b) => {
       if (selectedDirection === 'ascending') {
         if (a[selectedSort] < b[selectedSort]) return -1;
         if (a[selectedSort] > b[selectedSort]) return 1;
@@ -74,31 +122,53 @@ const TeacherLib =  () => {
       }
     });
   
-    setBooks(sortedMaterials); // Update the sorted data
+    setFilterdBooks(sortedMaterials);
+    
   };
   
 
-  return (<div className='page'>
-    <div className='lNav'><TeacherNavbar></TeacherNavbar></div>
-      <div className='content'>
+
+
+ 
+
+  return (<div>
+    <div><TeacherNavbar></TeacherNavbar></div>
+    <div className='content'> 
       <select name="sort" id="sort" value={sort} onChange={handleChangeSort}>
-          <option value="title" selected>Titel</option>
-          <option value="avgscore">Score</option>
-          <option value="lendcount">Aantal keer uitgeleend</option>
-          <option value="available">Beschikbaar</option>
-          <option value="place">Locatie</option>
-        </select>
-        <select name="sortDirection" id="sortDirection" value={sortDirection} onChange={handleChangeDirection}>
-          <option value="ascending" selected>Oplopen</option>
-          <option value="descending">Aflopend</option>
-        </select>
-          {showAll ? 
-      books.map((book) => (
-        <div key={book.title}>
-          <h3 onClick={() => { setSelectedBook(book); setShowAll(false); }} className='item'>{book.title}</h3>
-        </div>
-      ))
-      : <div>
+        <option value="title" >Titel</option>
+        <option value="avgscore">Score</option>
+        <option value="lendcount">Aantal keer uitgeleend</option>
+        <option value="available">Beschikbaar</option>
+        <option value="place">Locatie</option>
+      </select>
+      <select name="sortDirection" id="sortDirection" value={sortDirection} onChange={handleChangeDirection}>
+        <option value="ascending">Oplopen</option>
+        <option value="descending">Aflopend</option>
+      </select>
+      <select name='filter' id='filter' value={filter} onChange={handleChangeFilter}>
+        <option value="none">Geen filter</option>
+        <optgroup label='Beschikbaarheid' id='available'>
+          <option value="1">Beschikbaar</option>
+          <option value="0">Onbeschikbaar</option>
+        </optgroup>
+        <optgroup label='Locatie' id='place'>
+          {locations.map(location => <option value={location}>{location}</option>)}
+        </optgroup>
+        <optgroup label='Niveau' id='readinglevel'>
+        {readinglevels.map(readinglevel => <option value={readinglevel}>{readinglevel}</option>)}
+        </optgroup>
+      </select>
+
+      {showAll ? <div className='itemList'> { filterdBooks.map((book) => (
+      <li className='bookItem'>
+        <img src={book.descr.cover} alt="" className='cover'/>
+        <h3 onClick={() => { setSelectedBook(book); setShowAll(false); }} >{book.title}</h3>
+      </li>
+    ))}}
+        
+      </div>
+
+        : <div>
           <h2>{selectedBook.title}</h2>
           <h3>Auteur: {selectedBook.descr.author}</h3>
           <img src={selectedBook.descr.cover} alt="" />
@@ -107,10 +177,11 @@ const TeacherLib =  () => {
           <p>{selectedBook.lendoutto ? `Is uitgeleend door: ${selectedBook.lendoutto}` : ''}</p>
           <button onClick={() => setShowAll(true)} className="button">Toon alle boeken</button>
         </div>
-        }
-      </div>
+      }
     </div>
+  </div>
   )
 };
+
 
 export default TeacherLib;
