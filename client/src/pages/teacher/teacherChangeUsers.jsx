16a590be3733e3ecdc5a35272d729031a60098eb
WoutNerd <96572746+WoutNerd@users.noreply.github.com>
2024-06-06 14:41:13 +0200

    final push

--- client/src/pages/teacher/teacherChangeUsers.jsx
+++ client/src/pages/teacher/teacherChangeUsers.jsx
@@ -2,9 +2,17 @@ import { useEffect, useState } from 'react';
 import '../../App.css'
 import { Title, checkUser, getCookie, post } from '../../functions';
 import TeacherNavbar from './teacherNavbar';
+import { useNavigate } from 'react-router-dom';
 
 const TeacherChangeUser = () => {
   const [user, setUser] = useState([])
+
+  const navigate = useNavigate();
+  
+
+  const redirectToPage = (path) => {
+    navigate(path); // Use navigate to go to the specified path
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,30 +25,56 @@ const TeacherChangeUser = () => {
   Title('Bewerk gebruiker '+user.firstname + ' ' + user.lastname)
   checkUser(1)
 
-    const handleSubmit = (event) =>{
+    const handleSubmit = async (event) => {
       event.preventDefault()
+      const sessionid = getCookie('sessionId')
+      const userid = user.userid
+
+      let newName = document.getElementById('name').value
+      let newSurname = document.getElementById('surname').value
+      let newClss = document.getElementById('clss').value
+      let newNum = document.getElementsByTagName('clssNum').value
+      let newReadinglvl = document.getElementById('readinglvl').value
+
+    
+      if(newName == null) newName = user.firstname
+      if(newSurname == null) newSurname = user.lastname
+      if(newClss == null) newClss = user.class
+      if(newNum == null) newNum = user.classnum
+      if(newReadinglvl == null) newReadinglvl = user.readinglevel
+
+      const keys = ['firstname', 'lastname', 'class', 'classnum', 'readinglevel']
+      const values = [newName, newSurname, newClss, newNum, newReadinglvl]
+
+      const body = {sessionid, userid, keys, values}
+      post('/changeUser', body, 'changeuser').then((resp)=>resp.text().then((resp)=>{
+        if(resp == 'Statement executed correctly') alert('Gebruiker succesvol aangepast.')
+        else alert('De gebruiker is niet succesvol aangepast. Probeer later opnieuw.') 
+        redirectToPage('../leerkracht/leerlingen')
+      }))
+      
     }
 
     const renderForm = (
         <div className="form">
           <form onSubmit={handleSubmit}>
             <div className="input-container">
-              <input type="text" name="name" required placeholder="Voornaam" className="login" autoFocus />
+              <input type="text" id="name"  placeholder="Voornaam" className="login" autoFocus />
             </div>
             <div className="input-container">
-              <input type="text" className="login" name="surname" required placeholder="Achternaam" />
+              <input type="text" className="login" id="surname"  placeholder="Achternaam" />
             </div>
             <div className="input-container">
-              <input type="text" className="login" name="clss" required placeholder="Klas" />
+              <input type="text" className="login" id="clss"  placeholder="Klas" />
             </div>
             <div className="input-container">
-              <input type="text" className="login" name="clssNum" required placeholder="Klasnummer" />
+              <input type="text" className="login" id="clssNum"  placeholder="Klasnummer" />
             </div>
             <div className="input-container">
-              <input type="text" className="login" name="readinglvl" required placeholder="Leesniveau" />
+              <input type="text" className="login" id="readinglvl"  placeholder="Leesniveau" />
             </div>
             <div className="button-container">
-              <input type="submit" value={"Login"} className="login-button" />
+              <input type="submit" value={"Pas aan"} className="login-button" />
             </div>
           </form>
         </div>
